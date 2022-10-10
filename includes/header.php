68515be408025b8adc49caf33772abd6ba4a0d16
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-10-10 15:49:05 +0800

    setup(rtu-fs): first setup

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="File Manager System">
+    <meta name="keywords" content="Rizal Technological University File Manager System File Management">
+    <meta name="author" content="Erys">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php if(isset($page_title)) {
+                    echo $page_title;
+                } else {
+                    echo "Rizal Technological University";
+                }?>
+    </title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <script src="./assets/bootstrap/js/bootstrap.min.js" defer></script>
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="shortcut icon" href="./assets/images/rtu-favicon.png" type="image/x-icon">
+</head>
+<body>
