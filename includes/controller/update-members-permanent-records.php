c931dedd59f3276386c7711ed4d711c85311b3c8
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-10-15 21:04:38 +0800

    fix

--- includes/controller/update-members-permanent-records.php
+++ includes/controller/update-members-permanent-records.php
@@ -1,8 +1,12 @@
 <?php 
+
 session_start();
 
 if(isset($POST["submit"])) {
-    
+    require "../model/connection.php";
+    $student_number = $_POST["student_number"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
 
 } else {
     $_SESSION["status"] = "Not Allowed";
