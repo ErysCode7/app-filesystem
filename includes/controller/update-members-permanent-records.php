d73b0ba5002131339c36aac15f04d8de82123e92
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-10-15 21:03:05 +0800

    fix

--- includes/controller/update-members-permanent-records.php
+++ includes/controller/update-members-permanent-records.php
@@ -2,8 +2,11 @@
 session_start();
 
 if(isset($POST["submit"])) {
+    
 
 } else {
-    
+    $_SESSION["status"] = "Not Allowed";
+    header("Location: ../../member-permanent-records.php");
+    exit();
 }
 
