d7bb613781200e010a6178901590c3aeb7176e6e
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-10-15 21:20:13 +0800

    fix

--- includes/controller/update-members-permanent-records.php
+++ includes/controller/update-members-permanent-records.php
@@ -33,7 +33,7 @@ if(isset($POST["submit"])) {
         header("Location: ../../update-members-records.php?query=failed");
         exit();
     } else {
-        mysqli_stmt_bind_param($stmt, "ssssssssssssssss", $student_number, $first_name, $last_name, $birthday, );
+        mysqli_stmt_bind_param($stmt, "ssssssssssssssss", $student_number, $first_name, $last_name, $birthday, $contact_number, );
         mysqli_stmt_execute($stmt);
         $_SESSION["status-success"] = "Updated Successfully!";
         header("Location: ../../update-members-records.php?update=success");
