6af2bba5b64c065c5e253ceb071ff31963af0e67
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-10-19 20:26:24 +0800

    features: create-members

---
+++ pages/create-members-records.php
@@ -0,0 +1,13 @@
+<?php
+  require "../includes/authentication/authentication.php";
+?>
+
+<?php
+ require "../includes/view/header.php";
+?>
+
+
+
+
+
+<?php include "../includes/view/footer.php" ?>
