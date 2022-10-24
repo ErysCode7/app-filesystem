3db061cad25f46833965277db64e2debfa494235
Erys Mozo <69915035+ErysCode7@users.noreply.github.com>
2022-10-24 15:51:07 +0800

    fix

--- includes/controller/update-members-permanent-records.php
+++ includes/controller/update-members-permanent-records.php
@@ -14,6 +14,7 @@ if(isset($_POST["submit"])) {
     $curriculum_year = $_POST["curriculum_year"];
     $date_of_membership = $_POST["date_of_membership"];
     $active_status = $_POST["active_status"];
+    $address = $_POST["address"];
 
     $fathers_name = $_POST["fathers_name"];
     $fathers_occupation = $_POST["fathers_occupation"];
@@ -23,7 +24,7 @@ if(isset($_POST["submit"])) {
     $mothers_occupation = $_POST["mothers_occupation"];
     $mothers_phone_number = $_POST["mothers_phone_number"];
     
-    $sql = "UPDATE members_permanent_records SET student_number = ?, first_name = ?, last_name = ?, birthday = ?, contact_number = ?, troupe = ?, course = ?, curriculum_year = ?, date_of_membership = ?, active_status = ?, fathers_name = ?, fathers_occupation = ?, fathers_phone_number = ?, mothers_name = ?, mothers_occupation = ?, mothers_phone_number = ?  WHERE student_number = '$student_number';";
+    $sql = "UPDATE members_permanent_records SET student_number = ?, first_name = ?, last_name = ?, birthday = ?, contact_number = ?, troupe = ?, course = ?, curriculum_year = ?, date_of_membership = ?, address = ?, active_status = ?, fathers_name = ?, fathers_occupation = ?, fathers_phone_number = ?, mothers_name = ?, mothers_occupation = ?, mothers_phone_number = ?  WHERE student_number = '$student_number';";
 
     $stmt = mysqli_stmt_init($con);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
@@ -31,7 +32,7 @@ if(isset($_POST["submit"])) {
         header("Location: ../../pages/update-members-records.php?query=failed");
         exit();
     } else {
-        mysqli_stmt_bind_param($stmt, "ssssssssssssssss", $student_number, $first_name, $last_name, $birthday, $contact_number, $troupe, $course, $curriculum_year, $date_of_membership, $active_status, $fathers_name, $fathers_occupation, $fathers_phone_number, $mothers_name, $mothers_occupation, $mothers_phone_number);
+        mysqli_stmt_bind_param($stmt, "sssssssssssssssss", $student_number, $first_name, $last_name, $birthday, $contact_number, $troupe, $course, $curriculum_year, $date_of_membership, $address, $active_status, $fathers_name, $fathers_occupation, $fathers_phone_number, $mothers_name, $mothers_occupation, $mothers_phone_number);
         mysqli_stmt_execute($stmt);
         $_SESSION["status-success"] = "Updated Successfully!";
         header("Location: ../../pages/members-permanent-records.php?update=success");
