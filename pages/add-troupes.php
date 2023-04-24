bc528449c06763fd1833c70cdfa3d3b6c2878139
Lloyd Carie Mendoza Obligado <lobligado@intercommerce.com.ph>
2023-04-24 23:18:23 +0800

    Add troupes CRUD features

--- pages/add-troupes.php
+++ pages/add-troupes.php
@@ -96,8 +96,8 @@
                 <div data-i18n="Members Permanent Records">Members Permanent Records</div>
                 </a>
             </li>
-            
-            <!-- EVENTS -->
+
+             <!-- EVENTS -->
             <li class="menu-item">
                 <a href="./events.php" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-calendar-event"></i>
@@ -105,13 +105,14 @@
                 </a>
             </li>
 
-             <!-- TROUPES -->
+            <!-- TROUPES -->
             <li class="menu-item active">
                 <a href="./troupes.php" class="menu-link">
                 <i class="menu-icon tf-icons bx bx-location-plus"></i>
                 <div data-i18n="Troupes">Troupes</div>
                 </a>
-            </li> 
+            </li>
+
 
             </ul>
         </aside>
@@ -119,24 +120,18 @@
 
         <!-- Layout container -->
         <div class="layout-page">
-          <!-- Navbar -->
 
-          <?php require "../includes/view/navbar.php"; ?>
-
-          <!-- / Navbar -->
-
+        <!-- / Navbar -->
+        <?php require "../includes/view/navbar.php"; ?>
+        <!-- / Navbar -->
+        
           <!-- Content wrapper -->
           <div class="content-wrapper">
             <!-- Content -->
 
             <div class="container-xxl flex-grow-1 container-p-y">
-              <h1 class="fw-bold py-3 mb-4">List of Troupes</h1>
 
-            
-
-              <hr class="my-5" />
-
-            
+              <h1 class="fw-bold py-3 mb-4">Add New Troupes</h1>
 
               <?php
                   if(isset($_SESSION["status"])) {
@@ -153,94 +148,34 @@
               </div>
               <?php unset($_SESSION["status-success"]); } ?>
 
-              <!-- Hoverable Table rows -->
-              <div class="card">
-                
-                <h5 class="card-header">Troupes</h5>
-                <div style="position: absolute; right: 20px; top: 20px;">
-                <a href="./add-troupes.php">
-                  <button class="btn btn-primary">
-                    <i class="menu-icon tf-icons bx bx-plus"></i> Add Troupes</button></a> 
-                </div>
-                <div class="table-responsive">
-                  <table class="table table-hover">
-                    <thead>
-                      <tr>
-                        <th>Details</th>
-                        <th>Troupes</th>
-                        <th>Actions</th>
-                      </tr>
-                    </thead>
-                    <?php 
-                      require "../includes/model/connection.php";
-                      $sql = "SELECT * FROM troupes order by id desc";
-                      $result = $con->query($sql);
-                    ?>
-                    <?php while($row = $result->fetch_assoc()) { ?>
-                    <tbody class="table-border-bottom-0">
-                      <tr>
-                        <td><a href="./view-troupes.php?id=<?= $row["id"]; ?>">View Troupes</td>
-                        <td>
-                            <?php echo $row["name"]; ?>
-                        </td>
-                  
-                        <td class="d-flex align-items-center gap-2">
-                          <!-- UPDATE -->
-                          <form action="update-troupes.php" method="post">
-                            <!-- UPDATING FIELDS PASSING IT TO ANOTHER FORM -->   
-                            <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
-                            <input type="hidden" name="name" value="<?php echo $row["name"]; ?>">
+              <hr class="my-2" />
 
-                            <button class="btn btn-primary" type="submit"  name="submit"><i class="bx bx-edit-alt me-1"></i> Edit</button>
-                          </form>
-                          <!-- DELETE -->
-                          <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-                            <!-- Button trigger delete modal -->
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="<?php echo $row["id"]; ?>" onclick="confirmDelete(this);" ><i class="bx bx-trash me-1" ></i> Delete</butto>
-                            <input type="hidden" name="id" value="<?php echo $row["id"]; ?>">
-                          </form> 
-                        </td>
-                      </tr>
-                    </tbody>
-                    <?php } ?>     
-                  </table>
-                </div>
-            </div>
+                <div class="card-body">
+                    <form action="../includes/controller/troupes/create-troupes.php" method="post">
 
-            <script>
-              function confirmDelete(self) {
-                const id = self.getAttribute("data-id");
 
-                document.getElementById("form-delete-user").id.value = id;
-                $("#myModal").modal("show");
+                        <div class="row mb-3">
+                            <label class="h4" for="troupes_name">Troupes</label>
+                            <div class="">
+                                <input type="text" name="troupes_name" id="troupes_name" class="form-control py-3" style="color: black; font-size: 18px;" />
+                            </div>
+                        </div>
+                    
+                        <div class="row">
+                            <div class="col-sm-10">
+                                <button type="submit" name="submit" class="btn btn-primary" style="width: 300px;">Add Event</button>
+                            </div>
+                        </div>
+                       
+                      
 
-                
-              }
-            </script>
 
-               <!-- MODAL FOR DELETE  -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Delete Record</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                  </div>
-                <div class="modal-body">
-                    Would you like to delete this record? 
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <form action="../includes/controller/troupes/delete-troupes.php" method="post" id="form-delete-user">         
-                      <button type="submit" name="submit" class="btn btn-danger">Delete</button>
-                      <input type="hidden" name="id">
                     </form>
-                  </div>
                 </div>
-              </div>
+            
             </div>
 
-          </div>
+        </div>
           
           <!--/ Hoverable Table rows -->
           
